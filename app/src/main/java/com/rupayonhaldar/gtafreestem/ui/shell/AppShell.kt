6954473600237eb9163068cdb2ebbe7aaaa30435
Rupayon Haldar <80724680+rupayon123@users.noreply.github.com>
2026-09-07 19:58:31 -0400

    Refine nav selection border stroke widths

--- app/src/main/java/com/rupayonhaldar/gtafreestem/ui/shell/AppShell.kt
+++ app/src/main/java/com/rupayonhaldar/gtafreestem/ui/shell/AppShell.kt
@@ -355,7 +355,7 @@ private fun PrimaryNavigationBar(
                             )
                             .border(
                                 border = BorderStroke(
-                                    width = if (isSelected || isPressed) 0.9.dp else 0.dp,
+                                    width = if (isSelected || isPressed) 0.85.dp else 0.dp,
                                     color = selectedPillBorderColor,
                                 ),
                                 shape = activeItemShape,
@@ -893,7 +893,7 @@ private fun PrimaryNavigationRail(
                             )
                             .border(
                                 border = BorderStroke(
-                                    width = if (isSelected || isPressed) 0.8.dp else 0.dp,
+                                    width = if (isSelected || isPressed) 0.75.dp else 0.dp,
                                     color = selectedPillBorderColor,
                                 ),
                                 shape = activeItemShape,
